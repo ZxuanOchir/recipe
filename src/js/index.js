86c8f61ebbcd24d17b5c51ac7f1cc2f196f06860
Ochir <zxuan763@gmail.com>
2023-12-30 16:05:15 +0800

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,6 @@
-const arr = [23,44,12];
+// // fetch();
+// require("@babel/polyfill");
+import Search from "./model/Search";
+let search = new Search('pasta');
 
-let myFunc = a => {
-    console.log(`too :${a}`);
-}
-const arr2 = [...arr, 44, 1223];
-myFunc(arr2[1]);
-let x, y, z;    // Statement 1
-x = 5;          // Statement 2
-y = 6;          // Statement 3
-z = x + y
+search.doSearch().then(r => console.log(r));
--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
   entry: './src/js/index.js',
   mode: "production",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/main.js',
+    path: path.resolve(__dirname, 'docs'),
   },
   devServer: {
-    static: './dist',
+    static: './docs',
   },
   plugins: [
     new HtmlWebpackPlugin({
